350561bce8644d07fa5257b0ee66150ea835efda
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2025-05-08 09:35:50 +0700

    Update CRUD_23090043_C_2025.java

--- src/main/java/com/mycompany/p_uts_23090043_c_2025/CRUD_23090043_C_2025.java
+++ src/main/java/com/mycompany/p_uts_23090043_c_2025/CRUD_23090043_C_2025.java
@@ -53,14 +53,13 @@ public class CRUD_23090043_C_2025 {
                         col.insertOne(doc2);
                         
         Document doc3 = new Document();
-                        doc3.put("nama", "Honda WRV");
+                        doc3.put("nama", "Honda BRV");
                         doc3.put("warna", "putih");
-                        doc3.put("harga", 190000000);
+                        doc3.put("harga", 380000000);
                         col.insertOne(doc3);
         System.out.println("Data Tersimpan");
         
         // mendapatkan id dari data yang telah ditambahkan
-        
         ObjectId id = new ObjectId(doc.get("_id").toString());
         
         //melihat data
@@ -75,12 +74,12 @@ public class CRUD_23090043_C_2025 {
         document.put("nama", "Honda BRV");
         document.put("warna", "abu-abu");
         document.put("harga", 302000000);
-        Document updated = new Document("$Set", document);
+        Document updated = new Document("$set", document);
         UpdateResult hasilEdit = col.updateOne(eq("_id",id),updated);
         System.out.println(hasilEdit.getModifiedCount());
         
         //menghapus data
-        col.deleteOne(eq("_id", id));
+//        col.deleteOne(eq("_id", id));
 
         //mencari dokumen berdasarkan id 
         Document Docu = col.find(eq("_id", id)).first();
